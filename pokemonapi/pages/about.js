3bc057f8459ea78d9e20bc12193a0140572d724a
Eduardo <edalvarez2015@gmail.com>
2021-03-06 17:09:40 -0600

    Installed next.js + Tailwind CSS

---
+++ pokemonapi/pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque
+        ipsa qui non delectus deserunt atque suscipit optio quibusdam natus
+        porro ducimus reprehenderit veniam voluptatum tenetur neque laborum,
+        totam illum explicabo?
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pokemonapi/pages/index.js
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1 className="rounded bg-red-500 text-white">Welcome to Next.js</h1>
+    </div>
+  );
+}
